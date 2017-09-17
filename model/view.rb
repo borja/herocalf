dc59f3a2f9531e93d2944c37ad2234c80067e48e
Borja Martín Fernández <bmartinf@axpe.com>
2017-09-17 22:41:56 +0200

    Add Jotún

---
+++ model/view.rb
@@ -0,0 +1,31 @@
+# Main view class
+# Used for Bootstrap components management
+# Documentation for v.4 https://getbootstrap.com/docs/4.0/examples
+class Vista < Hash
+  # TODO: Non default carousel and breadcrumb should be accesor
+  attr_accessor :ruta, :title, :template
+
+  def initialize(args)
+    args.each do |k, v|
+      instance_variable_set("@#{k}", v) unless v.nil?
+    end
+  end
+
+  def breadcrumb?
+    ruta != 'inicio' # default
+    # TODO: Missing non-default behaviour
+  end
+
+  def carousel?
+    ruta == 'inicio'
+    # TODO: Renegados
+  end
+  
+  def breadcrumb
+    if breadcrumb?
+      [] # TODO: This should return an array
+    else
+      false
+    end
+  end
+end
