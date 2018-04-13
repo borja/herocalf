2d4e967bcaee039588ffca57cdaa14b50a2912e1
Borja Martín Fernández <bmartinf@axpe.com>
2018-04-13 11:40:41 +0200

    Fix rubocop offenses

--- model/heroes.rb
+++ model/heroes.rb
@@ -1,4 +1,4 @@
-# encoding: UTF-8
+# Heros filtering and searching methods
 
 def heros
   total_heros = Dir['./data/heroes/*'].length
@@ -12,29 +12,32 @@ def heros
 end
 
 def filter_heros(criteria, palabra)
-  filtered_heros = case criteria
-  when 'status'
-    lista_status = case palabra
-      when 'licenciados'  then 'retirado'
-      when 'heroes'       then 'activo'
-      when 'ausentes'     then 'ausente'
-      when 'reservistas'  then 'reserva'
-      when 'extranjeros'  then 'extranjero'
+  filtered_heros =
+    case criteria
+    when 'status'
+      # TODO: This should be API requests.
+      lista_status =
+        case palabra
+        when 'licenciados'  then 'retirado'
+        when 'heroes'       then 'activo'
+        when 'ausentes'     then 'ausente'
+        when 'reservistas'  then 'reserva'
+        when 'extranjeros'  then 'extranjero'
+        end
+
+      heros.select { |h| h.estado.downcase.include?(lista_status) }
+    when 'heroe'
+      heros.select { |h| h.name.downcase.include?(palabra) }
+    when 'jugador'
+      heros.select { |h| h.jugador.downcase.include?(palabra) }
+    when 'raza'
+      heros.select { |h| h.raza.downcase.include?(palabra) }
+    when 'mascota'
+      con_mascota = heros.reject { |h| h.pet.nil? }
+      con_mascota.select { |h| h.pet.name.downcase.include?(palabra) }
     end
 
-    heros.select {|h| h.estado.downcase.include?(lista_status) }
-  when 'heroe'
-    heros.select {|h| h.name.downcase.include?(palabra) }
-  when 'jugador'
-    heros.select {|h| h.jugador.downcase.include?(palabra) } 
-  when 'raza'
-    heros.select {|h| h.raza.downcase.include?(palabra) }
-  when 'mascota'
-    con_mascota = heros.reject {|h| h.pet.nil? }
-    con_mascota.select {|h| h.pet.name.downcase.include?(palabra)}
-  end
-
-  filtered_heros.sort_by { |h| [h.nivel,h.reputacion] }.reverse
+  filtered_heros.sort_by { |h| [h.nivel, h.reputacion] }.reverse
 end
 
 def hero(id)
@@ -45,18 +48,20 @@ def jugadores
   (heros + renegados).map(&:jugador).uniq
 end
 
-def narrados(n)
-  narrados = heros.select { |p| p.historia if p.historia == n }
+def narrados(historiados)
+  narrados = heros.select { |p| p.historia if p.historia == historiados }
   narrados.sort_by { |p| p.premio ? p.premio : 3 }
 end
 
-def nomalize_pj(pj)
+def nomalize_pj(personaje)
   normalization = {
     '%C3%A1' => 'á',
     '%C3%A9' => 'é',
     '%C3%AD' => 'í',
     '%C3%B3' => 'ó',
-    '%C3%BA' => 'ú',
+    '%C3%BA' => 'ú'
   }
-  pj.gsub(/%C3%A1|%C3%A9|%C3%AD|%C3%B3|%C3%BA/) { |match| normalization[match] }
+  personaje.gsub(/%C3%A1|%C3%A9|%C3%AD|%C3%B3|%C3%BA/) do |match|
+    normalization[match]
+  end
 end
