9003573b243020b4cc514926e0c4eee2e0079911
Borja Martín Fernández <borjamartinfernandez@gmail.com>
2019-05-01 12:00:57 +0200

    Sanitize yaml_loader with rubocopo

--- lib/yaml_loader.rb
+++ lib/yaml_loader.rb
@@ -1,10 +1,13 @@
+# frozen_string_literal: true
+
 # YAML Loader helper
 # TODO: "./lib" seems to be wrong place to handle this method
 # TODO: Test if this requirement is redundant
 require 'yaml'
 
 # Main fake DB loading method
-def load_yaml(path) # Path must be in /data
+# Path must be in /data
+def load_yaml(path)
   # path must be a yaml file path
   YAML.load_file(File.join(__dir__, "../data/#{path}.yml"))
   # YAML keys will be allways strings (never symbols)
