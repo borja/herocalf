ecb20ec55f2eccbc9f01e135eb324d358fdb75bf
Borja Martín Fernández <borjamartinfernandez@gmail.com>
2019-05-01 12:03:45 +0200

    Sanitize lib/test with rubocopo

--- lib/test.rb
+++ lib/test.rb
@@ -1,20 +1,25 @@
+# frozen_string_literal: true
+
 # Dictionary for the character test (14 questions)
 def preguntas
   load_yaml('personaje/test')
 end
 
 # Recoge los valores de las respuestas y los agrupa
-def collect_values(hashes) # An array of hashes should be provided
+def collect_values(hashes)
+  # An array of hashes should be provided
   {}.tap { |r| hashes.each { |h| h.each { |k, v| (r[k] ||= []) << v } } }
 end
 
 # After question num 14, this will turn on
-def respuestas(codigo) # Code from URL
+def respuestas(codigo)
+  # Code from URL
   if codigo.to_s.length == preguntas.count # Check size (14)
     preguntas.map do |p|
       r = codigo[p['id']].to_i # respuesta segun el codigo
       p['respuestas'][r].last  # valores de puntuacion segun codigo
-    end                        # devuelve {"enano"=>2, "elfo"=>1} 14 veces
+      # devuelve {"enano"=>2, "elfo"=>1} 14 veces
+    end
   else
     redirect :error # If not 14, then raise error
   end
